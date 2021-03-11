6b055e9f773a73e621f65e3dbf9de0558737a506
Sarah <sarah.pflug@shopify.com>
2021-03-11 21:15:30 +0000

    initial upload, not sure if my formatting is correct

---
+++ breedFetcher.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const breedName = process.argv[2];
+
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
+
+request( url, (error, response, body) => {
+// console.log('body:', body);
+// console.log('statusCode:', response && response.statusCode);
+// console.log(typeof body)
+if (error){
+  return console.log('THIS IS ERROR:',JSON.stringify(error))
+} else {
+  const data = JSON.parse(body);
+// console.log(data);
+// console.log(typeof data);
+const breed = data[0];
+if (breed){
+  console.log(breed.description)
+  } else {
+    console.log (`error: ${breedName} not found`);
+  }
+}
+});
+
+
